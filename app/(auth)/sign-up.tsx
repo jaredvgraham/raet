866874963138adfbc050f5891ad33a1e894d5011
JaredvGraham <jgram153@icloud.com>
2024-08-25 17:07:03 -0400

    sign up wit google button styles

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -5,9 +5,15 @@ import {
   Image,
   TextInput,
   KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
 } from "react-native";
 import React, { useState } from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { router } from "expo-router";
+import OAuth from "@/components/OAuth";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
@@ -16,22 +22,30 @@ const SignUp = () => {
     password: "",
   });
 
+  const onSignUp = () => {
+    console.log(formData);
+  };
+
   return (
     <ScrollView className="flex-1 bg-white">
-      <KeyboardAvoidingView behavior="padding" className="flex-1 p-6">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        className="flex-1 p-6"
+      >
         <View className="flex items-center mb-12">
           <Image
             source={require("../../assets/r-logo.png")}
             className="w-24 h-24 mb-4"
           />
           <Text className="text-3xl font-light text-gray-900">
-            Create an account
+            Create an account!
           </Text>
         </View>
 
         <View className="space-y-6">
           {/* Name Input */}
-          <View className="flex-row items-center border border-gray-300 rounded-full px-4 py-2 bg-gray-100">
+
+          <View className="flex-row items-center border border-gray-300 rounded-full px-4 py-2 bg-gray-100 focus:border-blue-300">
             <View className="mr-2">
               <Icon name="user" size={20} color="gray" />
             </View>
@@ -40,12 +54,12 @@ const SignUp = () => {
               placeholderTextColor="gray"
               value={formData.name}
               onChangeText={(text) => setFormData({ ...formData, name: text })}
-              className="flex-1 text-start text-gray-800"
+              className="flex-1 text-start text-gray-800 "
             />
           </View>
 
           {/* Email Input */}
-          <View className="flex-row items-center border border-gray-300 rounded-full px-4 py-2 bg-gray-100">
+          <View className="flex-row items-center border border-gray-300 rounded-full px-4 py-2 bg-gray-100 focus:border-blue-300">
             <View className="mr-2">
               <Icon name="envelope" size={20} color="gray" />
             </View>
@@ -60,7 +74,7 @@ const SignUp = () => {
           </View>
 
           {/* Password Input */}
-          <View className="flex-row items-center border border-gray-300 rounded-full px-4 py-2 bg-gray-100">
+          <View className="flex-row items-center border border-gray-300 rounded-full px-4 py-2 bg-gray-100 focus:border-blue-300">
             <View className="mr-2">
               <Icon name="lock" size={20} color="gray" />
             </View>
@@ -75,6 +89,27 @@ const SignUp = () => {
               className="flex-1 text-start text-gray-800"
             />
           </View>
+
+          {/* Sign Up Button */}
+
+          <TouchableOpacity onPress={onSignUp} className="shadow-2xl">
+            <View className="flex items-center justify-center w-full bg-blue-500 rounded-full py-3 ">
+              <Text className="text-white text-lg font-semibold">Sign Up</Text>
+            </View>
+          </TouchableOpacity>
+
+          {/* OAuth*/}
+          <OAuth />
+
+          {/* Sign In */}
+          <View className="ml-2">
+            <Text className="text-gray-500">Already have an account?</Text>
+            <TouchableOpacity onPress={() => router.push("/(auth)/sign-in")}>
+              <Text className="text-blue-500 font-bold ">Sign In</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Verification Modal */}
         </View>
       </KeyboardAvoidingView>
     </ScrollView>
---
+++ components/OAuth.tsx
@@ -0,0 +1,34 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+
+const OAuth = () => {
+  const handleGoogleSignIn = () => {
+    console.log("Google Sign In");
+  };
+
+  return (
+    <View>
+      <View className="flex-row justify-center items-center mt-4">
+        <View className="flex-1 h-px bg-gray-300" />
+        <Text className="text-gray-500 mx-3">or</Text>
+        <View className="flex-1 h-px bg-gray-300" />
+      </View>
+
+      {/* Sign in with Google */}
+      <TouchableOpacity
+        className="my-5 flex-row items-center justify-center w-full bg-white rounded-full py-3 border border-gray-300"
+        onPress={handleGoogleSignIn}
+      >
+        <Image
+          source={require("../assets/google-icon.webp")}
+          className="w-6 h-6 mr-2"
+        />
+        <Text className="text-black text-lg font-semibold">
+          Continue with Google
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default OAuth;
