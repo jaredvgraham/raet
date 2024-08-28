c21bbd21aa67f5f6f9071d66e8b8727730565042
Jared Graham <jaredgraham@mac-mini.mynetworksettings.com>
2024-08-28 19:44:14 -0400

    first

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
