76aa624651002deabcd031d14d323f7113d12b35
JaredvGraham <jgram153@icloud.com>
2025-02-07 12:15:35 -0500

    mongo bug

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGO_URI!, {
+      dbName: "raet-db",
+      bufferCommands: false,
+      connectTimeoutMS: 30000,
+      autoIndex: true,
+    });
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
